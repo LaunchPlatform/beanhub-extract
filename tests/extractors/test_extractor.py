0e06c13463d7e8daf58709a2a3ecd01d2d2bec47
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-23 16:59:04 -0700

    Add test for extractor detect

---
+++ tests/extractors/test_extractor.py
@@ -0,0 +1,25 @@
+import pathlib
+
+import pytest
+
+from beanhub_extract.extractors import detect_extractor
+
+
+@pytest.mark.parametrize(
+    "input_file, extractor_name",
+    [
+        ("mercury.csv", "mercury"),
+        ("chase_credit_card.csv", "chase_credit_card"),
+        ("other.csv", None),
+        (pytest.lazy_fixture("zip_file"), None),
+    ],
+)
+def test_detect_extractor(
+    fixtures_folder: pathlib.Path, input_file: str, extractor_name: str | None
+):
+    with open(fixtures_folder / input_file, "rt") as fo:
+        extractor_cls = detect_extractor(fo)
+        if extractor_name is None:
+            assert extractor_cls is None
+        else:
+            assert extractor_cls.EXTRACTOR_NAME == extractor_name
